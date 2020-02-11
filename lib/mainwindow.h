fb143d6be81528f0e84fbeb6eacce9e731ed9889
Harry E. Allen, IV <hevansa98@gmail.com>
2020-02-11 13:50:06 -0600

    GUI built, still working on slots

--- lib/mainwindow.h
+++ lib/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../lib/employee.h"
+#include "../lib/fileio.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +16,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_CreateUserButton_clicked();
+
+    void on_SearchUserButton_clicked();
 
 private:
     Ui::MainWindow *ui;
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -13,3 +13,21 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_CreateUserButton_clicked()
+{
+    if(ui->EmpCheckBox->isChecked()){
+        std::cout << "This works checked!" << std::endl;
+    }else{
+        std::cout << "This works unchecked!" << std::endl;
+    }
+}
+
+void MainWindow::on_SearchUserButton_clicked()
+{
+    if(ui->EmpCheckBox->isChecked()){
+        std::cout << "This works checked!" << std::endl;
+    }else{
+        std::cout << "This works unchecked!" << std::endl;
+    }
+}
